be6c96ec6c829a133ec08b7da9e7380cb13035d7
VJansou <vincent.jansou18@gmail.com>
2024-06-19 10:44:12 +0200

    init

---
+++ lowrr/consts.py
@@ -0,0 +1,11 @@
+# Constantes utilisées dans le programme
+OUT_DIR = "out" # Répertoire de sortie par défaut
+
+LEVELS = 4 # Nombre de niveaux de résolution par défaut
+SPARSE_RATIO_THRESHOLD = 0.5 # Seuil de sparsité par défaut
+
+LAMBDA = 1.5 # Paramètre lambda par défaut
+RHO = 0.1 # Paramètre rho par défaut
+
+TRESHOLD = 1e-3 # Seuil de convergence par défaut
+MAX_ITER = 40 # Nombre d'itérations maximum par défaut
---
+++ lowrr/main.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+from lowrr.consts import *
+
